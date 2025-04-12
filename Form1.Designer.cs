7eac44b18109cea8a8197c1284be7883656a2047
hicrangunn <hhicrangun@gmail.com>
2025-04-12 15:50:04 +0300

    form1 ve designer kısmı yüklendi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,17 @@
-﻿namespace cafe_oto
+﻿namespace StokTakipApp
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.TextBox txtKategori;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView dgvUrunler;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +21,100 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.txtKategori = new System.Windows.Forms.TextBox();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.dgvUrunler = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUrunler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(30, 30);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.PlaceholderText = "Ürün Adı";
+            this.txtUrunAdi.Size = new System.Drawing.Size(150, 23);
+            // 
+            // txtKategori
+            // 
+            this.txtKategori.Location = new System.Drawing.Point(30, 60);
+            this.txtKategori.Name = "txtKategori";
+            this.txtKategori.PlaceholderText = "Kategori";
+            this.txtKategori.Size = new System.Drawing.Size(150, 23);
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(30, 90);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.PlaceholderText = "Miktar";
+            this.txtMiktar.Size = new System.Drawing.Size(150, 23);
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(30, 120);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.PlaceholderText = "Fiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(150, 23);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(200, 30);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 23);
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(200, 60);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(100, 23);
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(200, 90);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(100, 23);
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // dgvUrunler
+            // 
+            this.dgvUrunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUrunler.Location = new System.Drawing.Point(30, 160);
+            this.dgvUrunler.Name = "dgvUrunler";
+            this.dgvUrunler.RowTemplate.Height = 25;
+            this.dgvUrunler.Size = new System.Drawing.Size(440, 200);
+            this.dgvUrunler.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUrunler_CellClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.txtKategori);
+            this.Controls.Add(this.txtMiktar);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.dgvUrunler);
+            this.Name = "Form1";
+            this.Text = "Stok Takip";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUrunler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-        #endregion
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,81 @@
-namespace cafe_oto
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+SqlConnection baglanti = new SqlConnection("Server=.;Database=StokTakipDB;Trusted_Connection=True;");
+
+
+namespace StokTakipApp
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Server=localhost;Database=StokTakip;Trusted_Connection=True;");
+        SqlDataAdapter da;
+        DataTable dt;
+
         public Form1()
-        //MIYAVV
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        void Listele()
+        {
+            da = new SqlDataAdapter("SELECT * FROM Urunler", con);
+            dt = new DataTable();
+            da.Fill(dt);
+            dgvUrunler.DataSource = dt;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Urunler (UrunAdi, Kategori, Miktar, Fiyat) VALUES (@adi, @kategori, @miktar, @fiyat)", con);
+            cmd.Parameters.AddWithValue("@adi", txtUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@kategori", txtKategori.Text);
+            cmd.Parameters.AddWithValue("@miktar", txtMiktar.Text);
+            cmd.Parameters.AddWithValue("@fiyat", txtFiyat.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Listele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Urunler SET UrunAdi=@adi, Kategori=@kategori, Miktar=@miktar, Fiyat=@fiyat WHERE Id=@id", con);
+            cmd.Parameters.AddWithValue("@adi", txtUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@kategori", txtKategori.Text);
+            cmd.Parameters.AddWithValue("@miktar", txtMiktar.Text);
+            cmd.Parameters.AddWithValue("@fiyat", txtFiyat.Text);
+            cmd.Parameters.AddWithValue("@id", dgvUrunler.CurrentRow.Cells[0].Value);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM Urunler WHERE Id=@id", con);
+            cmd.Parameters.AddWithValue("@id", dgvUrunler.CurrentRow.Cells[0].Value);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Listele();
+        }
+
+        private void dgvUrunler_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtUrunAdi.Text = dgvUrunler.CurrentRow.Cells[1].Value.ToString();
+            txtKategori.Text = dgvUrunler.CurrentRow.Cells[2].Value.ToString();
+            txtMiktar.Text = dgvUrunler.CurrentRow.Cells[3].Value.ToString();
+            txtFiyat.Text = dgvUrunler.CurrentRow.Cells[4].Value.ToString();
+        }
     }
 }
+
