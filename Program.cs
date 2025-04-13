11f0b559c1a4b33bb596b6e30d40a18f19192b74
hicrangunn <hhicrangun@gmail.com>
2025-04-13 19:25:27 +0300

    program dosyası değişikliği

--- Program.cs
+++ Program.cs
@@ -3,15 +3,14 @@ using System.Windows.Forms;
 
 namespace StokTakipApp
 {
-    static class Program
+    internal static class Program
     {
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
     }
 }
+
